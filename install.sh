2a575339229a22e5498546c1fc7edd35396fd32f
Klaatu von Schlacker <klaatu@hackerpublicradio.org>
2011-05-02 10:07:21 -0400

    fixed install.sh for os x and other systems lacking a usr local bin and made trash return a cleaner friendlier message

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # install the humble but useful trash command
 
-cp ./trash /usr/local/bin/trash
-cp ./empty /usr/local/bin/empty
-chmod +x /usr/local/bin/trash /usr/local/bin/empty
+OS=$(uname)
+if [ "X$OS" == "XDarwin" ] 
+	then INSTDIR=/usr/bin
+	else INSTDIR=/usr/local/bin
+fi
+
+cp -f ./trash $INSTDIR/trash
+cp -f ./empty $INSTDIR/empty
+chmod +x $INSTDIR/trash $INSTDIR/empty
 
 PROCHK=$(cat /etc/profile | sed -n '/alias rm="trash"/p' )
 if [ "X$PROCHK" == "X" ]
@@ -11,7 +17,7 @@ if [ "X$PROCHK" == "X" ]
 	echo 'alias rm="trash"' >> /etc/profile
 	source /etc/profile
 fi
-CHECK=$(ls /usr/local/bin | grep trash)
+CHECK=$(ls $INSTDIR | grep trash)
 if [ "X$CHECK" == "X" ]
 	then echo "Install encountered some errors.  You might try copying the apps to your home directory (into ~/bin for instance) and use them locally."
 	else echo "Installation script complete."
