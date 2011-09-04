c28bd4ce3051d34ac0875a9bd7c86025b6cbee04
sxe <sxe@bluebox.pghfilmmakers.local>
2011-09-04 07:43:34 -0400

    commented out the alias block since not everyone who installs this will want rm aliased to trash. heck i dont even know if _I_ want that. what was i thinking?

--- install.sh
+++ install.sh
@@ -11,12 +11,18 @@ cp -f ./trash $INSTDIR/trash
 cp -f ./empty $INSTDIR/empty
 chmod +x $INSTDIR/trash $INSTDIR/empty
 
-PROCHK=$(cat /etc/profile | sed -n '/alias rm="trash"/p' )
-if [ "X$PROCHK" == "X" ]
-	then 
-	echo 'alias rm="trash"' >> /etc/profile
-	source /etc/profile
-fi
+########################################
+### uncomment the block below if you ###
+### want to alias 'rm' to 'trash'    ###
+########################################
+
+#PROCHK=$(cat /etc/profile | sed -n '/alias rm="trash"/p' )
+#if [ "X$PROCHK" == "X" ]
+#	then 
+#	echo 'alias rm="trash"' >> /etc/profile
+#	source /etc/profile
+#fi
+
 CHECK=$(ls $INSTDIR | grep trash)
 if [ "X$CHECK" == "X" ]
 	then echo "Install encountered some errors.  You might try copying the apps to your home directory (into ~/bin for instance) and use them locally."
